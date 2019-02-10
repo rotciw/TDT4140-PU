adb7cd6bc04b17ccbb5f346567509a88839d5ea7
Jesper Paulsen <jespergp@stud.ntnu.no>
2019-02-10 16:43:31 +0100

    Small changes to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,29 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   env: {
     browser: true,
     node: true
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs'
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
   ],
   // add your custom rules here
-  rules: {}
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': ['error', 'as-needed'],
+    // 'one-var': ['error', 'always'],
+    'dot-location': ['error', 'property'],
+    'eqeqeq': ['error', 'always'],
+    'no-multi-spaces': [2, { exceptions: { 'VariableDeclarator': true, 'ImportDeclaration': true } }],
+    'one-var': 0,
+    'indent': ['error', 2, { 'VariableDeclarator': { 'var': 2, 'let': 2, 'const': 3 } }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }],
+    'prefer-arrow-callback': ['error']
+  }
 }
