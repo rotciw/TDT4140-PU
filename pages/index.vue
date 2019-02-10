d80cc2ba6157871fc4f6d67f436e2954cc334b93
Jesper Paulsen <jespergp@stud.ntnu.no>
2019-02-10 19:35:02 +0100

    Merge branch 'develop'
    Merged develop to get some structure in master

--- pages/index.vue
+++ pages/index.vue
@@ -16,6 +16,7 @@
       <v-card>
         <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
+          Gruppe 30
         </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
