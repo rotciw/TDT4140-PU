0a1656fb81576e5acda20016271965f718a76fd6
nknjhbjyf <36053435+nknjhbjyf@users.noreply.github.com>
2019-02-12 12:01:29 +0100

    fet nettside

---
+++ pages/mathilde.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container>
+    <v-layout
+      align-space-around
+      justify-space-around
+      row
+      fill-height
+    >
+      <v-flex
+        xs4
+        lg4
+      >
+        <div class="red">
+          <h2 style="text-align: center">
+            :))))
+          </h2>
+        </div>
+      </v-flex>
+      <v-flex
+        xs4>
+        <div class="blue">
+          <h2 style="...">
+            Taco
+          </h2>
+        </div>
+      </v-flex>
+      <v-flex
+        xs4>
+        <div class="green">
+          <h2 style="...">
+            Guacamole
+          </h2>
+        </div>
+      </v-flex>
+    </v-layout>
+    <div class="text-xs-center">
+      <v-btn
+        large
+        color="success"
+        fab
+        raised
+      />
+      <v-icon dark>list</v-icon>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Mathilde'
+}
+</script>
+
+<style scoped>
+
+</style>
