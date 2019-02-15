f4790904ce704061a92f4edfa329ac09129017a8
Wictor Zhao <wictorz@stud.ntnu.no>
2019-02-15 14:13:05 +0100

    commiting changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,7 +14,7 @@
               <img src="https://randomuser.me/api/portraits/men/85.jpg">
             </v-list-tile-avatar>
 
-            <v-list-tile-content>
+            <v-list-tile-content >
               <v-list-tile-title>Sjef Sjefesen</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
@@ -55,9 +55,9 @@
       >
         <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
-      <v-toolbar-title v-text="title" />
+      <v-toolbar-title v-text="title"/>
     </v-toolbar>
-    <v-content>
+    <v-content class="bgColor">
       <v-container>
         <nuxt />
       </v-container>
@@ -100,3 +100,9 @@ export default {
   }
 }
 </script>
+
+<style>
+  .bgColor{
+    background-color: #D1DFD2;
+  }
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,73 @@
 <template>
-  $END$
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <img
+      src="logo-long.png"
+      height="200px"
+      contain
+      style="margin-bottom:50px"
+    >
+    <v-flex
+      xs12
+      sm5
+      md5
+    >
+      <!-- Kommentar -->
+      <div class="loginbox">
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              id="username"
+              class="roundedCorners"
+              prepend-inner-icon="person"
+              name="login"
+              label="Brukernavn"
+              type="text"
+            />
+            <v-text-field
+              id="password"
+              class="roundedCorners"
+              prepend-inner-icon="lock"
+              name="password"
+              label="Passord"
+              type="password"
+            />
+          </v-form>
+        </v-card-text>
+        <div class="text-xs-center">
+          <v-btn
+            large
+            color="#6BE096"
+            class="roundedCorners"
+          >
+            Logg inn
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-  export default {
-    name: "login"
+export default {
+  methods: {
   }
+}
 </script>
 
 <style scoped>
-
+  .loginbox{
+    height:300px;
+    width: 450px;
+    padding:30px;
+    background-color: #f5f5f5;
+    border-radius: 0px 36px 0px 36px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+  .roundedCorners {
+    border-radius: 0px 18px 0px 18px;
+  }
 </style>
