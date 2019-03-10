7d04dcf568ef7f593b6e2e25cabebff8e636340a
Jesper Paulsen <jesper@jesper.no>
2019-03-10 22:06:50 +0100

    La til et par cloud functions for å vise statistikk. Instalerte et par pakker i functions mappen.

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,188 @@
-/*  admin       = require('firebase-admin'),
-      auth        = require('basic-auth'),
+const admin       = require('firebase-admin'),
+      // auth        = require('basic-auth'),
       bodyParser  = require('body-parser'),
       cors        = require('cors')({ origin: true }),
       express     = require('express'),
       functions   = require('firebase-functions'),
-      Moment      = require('moment'),
+      moment      = require('moment'),
       request     = require('request'),
-      ses         = require('node-ses'),
+      // ses         = require('node-ses'),
       app         = express()
 
 app.use(bodyParser.json())
+// admin.initializeApp()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  let message = request.body.message1
+  response.send("Hello from Firebase! " + message)
+})
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 
 admin.initializeApp(functions.config().firebase);
+let db = admin.firestore();
 
-var db = admin.firestore(); */
+/*
+Cloud funksjon som regner ut hvor mange reservasjoner vi har hver hele time på restauranten. Statistikken runder ned til hver påbegynte klokketime, så reservasjoner
+som er fra f.eks 12:00 til 13:59 vil regnes som ekstra besøk kl 12 og 13, men ikke 14.
+ */
+exports.hourlyNumberOfReservations = functions.https.onRequest((request, response) => {
+  let hourlyStatistics = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  db.collection('reservations')
+    .get()
+    .then(reservations => {
+      return reservations.forEach(reservation => {
+        reservation = reservation.data()
+        const startTime = moment(reservation.startTime).format('H')
+        const endTime = moment(reservation.endTime).format('H')
+        for (let i = Number(startTime); i < Number(endTime) + 1; i++) {
+          hourlyStatistics[i - 1] = hourlyStatistics[i - 1] + 1
+        }
+      })
+    })
+    .then(() => {
+      return response.status(200).send(hourlyStatistics)
+    })
+    .catch(error => {
+      console.log('Klarte ikke å mounte timestatistikk for antall reservasjoner')
+      console.log(error)
+      response.send(error)
+    })
+})
+
+/*
+Cloud funksjon som regner ut hvor mange besøkende vi har hver hele time på restauranten. Statistikken runder ned til hver påbegynte klokketime, så reservasjoner
+som er fra f.eks 12:00 til 13:59 vil regnes som ekstra besøk kl 12 og 13, men ikke 14.
+ */
+exports.hourlyNumberOfPersons = functions.https.onRequest((request, response) => {
+  let hourlyStatistics = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  db.collection('reservations')
+    .get()
+    .then(reservations => {
+      return reservations.forEach(reservation => {
+        reservation = reservation.data()
+        const startTime = moment(reservation.startTime).format('H')
+        const endTime = moment(reservation.endTime).format('H')
+        for (let i = Number(startTime); i < Number(endTime) + 1; i++) {
+          hourlyStatistics[i - 1] = hourlyStatistics[i - 1] + reservation.numberOfPersons
+        }
+      })
+    })
+    .then(() => {
+      return response.status(200).send(hourlyStatistics)
+    })
+    .catch(error => {
+      console.log('Klarte ikke å mounte timestatistikk for antall besøkende')
+      console.log(error)
+      response.send(error)
+    })
+})
+/*
+Cloud funksjon som regner ut hvor mange reservasjoner vi har hver ukedag
+ */
+exports.dailyNumberOfReservations = functions.https.onRequest((request, response) => {
+  let dailyStatistics = [0, 0, 0, 0, 0, 0, 0]
+
+  db.collection('reservations')
+    .get()
+    .then(reservations => {
+      return reservations.forEach(reservation => {
+        reservation = reservation.data()
+        const weekday = Number(moment(reservation.startTime).day())
+        dailyStatistics[weekday - 1] = dailyStatistics[weekday - 1] + 1
+      })
+    })
+    .then(() => {
+      return response.status(200).send(dailyStatistics)
+    })
+    .catch(error => {
+      console.log('Klarte ikke å mounte timestatistikk for antall besøkende')
+      console.log(error)
+      response.send(error)
+    })
+})
+
+/*
+Cloud funksjon som regner ut hvor mange besøkende vi har hver ukedag
+ */
+exports.dailyNumberOfPersons = functions.https.onRequest((request, response) => {
+  let dailyStatistics = [0, 0, 0, 0, 0, 0, 0]
+
+  db.collection('reservations')
+    .get()
+    .then(reservations => {
+      return reservations.forEach(reservation => {
+        reservation = reservation.data()
+        const weekday = Number(moment(reservation.startTime).day())
+        dailyStatistics[weekday - 1] = dailyStatistics[weekday - 1] + Number(reservation.numberOfPersons)
+      })
+    })
+    .then(() => {
+      return response.status(200).send(dailyStatistics)
+    })
+    .catch(error => {
+      console.log('Klarte ikke å mounte timestatistikk for antall besøkende')
+      console.log(error)
+      response.send(error)
+    })
+})
 
 
+/*
+Cloud funksjon som regner ut hvor mange reservasjoner vi har hver måned
+ */
+
+exports.monthlyNumberOfReservations = functions.https.onRequest((request, response) => {
+  let monthlyStatistics = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  db.collection('reservations')
+    .get()
+    .then(reservations => {
+      return reservations.forEach(reservation => {
+        reservation = reservation.data()
+        const month = Number(moment(reservation.startTime).month())
+        monthlyStatistics[month - 1] = monthlyStatistics[month - 1] + 1
+      })
+    })
+    .then(() => {
+      return response.status(200).send(monthlyStatistics)
+    })
+    .catch(error => {
+      console.log('Klarte ikke å mounte timestatistikk for antall besøkende')
+      console.log(error)
+      response.send(error)
+    })
+})
+
+/*
+Cloud funksjon som regner ut hvor mange besøkende vi har hver måned
+TODO: Fikse denne
+ */
+
+exports.monthlyNumberOfPersons = functions.https.onRequest((request, response) => {
+  let monthlyStatistics = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  db.collection('reservations')
+    .get()
+    .then(reservations => {
+      return reservations.forEach(reservation => {
+        reservation = reservation.data()
+        const month = Number(moment(reservation.startTime).month())
+        monthlyStatistics[month - 1] = monthlyStatistics[month - 1] + reservation.numberOfPersons
+      })
+    })
+    .then(() => {
+      return response.status(200).send(monthlyStatistics)
+    })
+    .catch(error => {
+      console.log('Klarte ikke å mounte timestatistikk for antall besøkende')
+      console.log(error)
+      response.send(error)
+    })
+})
