59638027048a91fd7089249694b66b7e2deae702
olavmfa <olavmfa@stud.ntnu.no>
2019-03-11 14:35:20 +0100

    Startet på backend epostbekreftelse

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,16 @@
-/*  admin       = require('firebase-admin'),
-      auth        = require('basic-auth'),
+const  admin       = require('firebase-admin'),
       bodyParser  = require('body-parser'),
-      cors        = require('cors')({ origin: true }),
+      cors        = require('cors'),
       express     = require('express'),
       functions   = require('firebase-functions'),
-      Moment      = require('moment'),
+      moment      = require('moment'),
       request     = require('request'),
-      ses         = require('node-ses'),
-      app         = express()
+      app         = express(),
+      nodemailer = require('nodemailer');
+
 
 app.use(bodyParser.json())
+app.use(cors({origin: true }))
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -23,6 +24,90 @@ app.use(bodyParser.json())
 
 admin.initializeApp(functions.config().firebase);
 
-var db = admin.firestore(); */
+var db = admin.firestore();
 
+'use strict';
+
+
+app.post('/sendReservationEmail', (request, response) => {
+  console.log('sendt reservation confirmation email')
+
+  const { startTime, numberOfPersons, reservationID, email } = request.body
+  const gmailEmail = functions.config().gmail.email;
+  const gmailPassword = functions.config().gmail.password;
+  const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'tacosbetrippin',
+      pass: 'trippin123',
+    },
+  })
+  response.status(200).send(startTime)
+})
+
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+
+
+// Your company name to include in the emails
+// TODO: Change this to your app or company name to customize the email sent.
+const APP_NAME = 'Trippin Tacos'
+// [START sendWelcomeEmail]
+/**
+ * Sends a welcome email to new user.
+ */
+// [START onCreateTrigger]
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+// [END onCreateTrigger]
+  // [START eventAttributes]
+  const email = user.email; // The email of the user.
+  const displayName = user.displayName; // The display name of the user.
+  // [END eventAttributes]
+
+  return sendWelcomeEmail(email, displayName);
+});
+// [END sendWelcomeEmail]
+
+// [START sendByeEmail]
+/**
+ * Send an account deleted email confirmation to users who delete their accounts.
+ */
+// [START onDeleteTrigger]
+exports.sendByeEmail = functions.auth.user().onDelete((user) => {
+// [END onDeleteTrigger]
+  const email = user.email;
+  const displayName = user.displayName;
+
+  return sendGoodbyeEmail(email, displayName);
+});
+// [END sendByeEmail]
+
+// Sends a welcome email to the given user.
+function sendWelcomeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email,
+  };
+
+  // The user subscribed to the newsletter.
+  mailOptions.subject = `Welcome to ${APP_NAME}!`;
+  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
+  return mailTransport.sendMail(mailOptions).then(() => {
+    return console.log('New welcome email sent to:', email);
+  });
+}
+
+// Sends a goodbye email to the given user.
+function sendGoodbyeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email,
+  };
+
+  // The user unsubscribed to the newsletter.
+  mailOptions.subject = `Bye!`;
+  mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
+  return mailTransport.sendMail(mailOptions).then(() => {
+    return console.log('Account deletion confirmation email sent to:', email);
+  });
+}
 
