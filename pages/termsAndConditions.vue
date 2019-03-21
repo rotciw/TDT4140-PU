db1c3a0bfa48613573a97b744a638d80afe08b71
Wictor Zhao <wictorz@stud.ntnu.no>
2019-03-21 14:04:04 +0100

    termsandconditions

---
+++ pages/termsAndConditions.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-dialog
+      v-model="termsdialog"
+      max-width="800px"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="headline">Vilkår og betingelser</span>
+        </v-card-title>
+        <v-card-text>
+          Hehe
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+    <img
+      src="logo-long.png"
+      height="150px"
+      contain
+      style="margin-bottom:50px"
+    >
+    <v-spacer>
+    </v-spacer>
+    <v-btn
+      color="red darken-1"
+      flat
+      @click="close"
+    >
+      Avbryt
+    </v-btn>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .roundedCorners {
+    border-radius: 0px 18px 0px 18px;
+  }
+</style>
