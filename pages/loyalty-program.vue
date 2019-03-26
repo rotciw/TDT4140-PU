a3cbd0ebc8c8c48f475e8fd27826f99f632154ac
mathilde <mathith@stud.ntnu.no>
2019-03-26 10:57:46 +0100

    lagd lojalitetsprogram-side

---
+++ pages/loyalty-program.vue
@@ -0,0 +1,13 @@
+<template>
+</template>
+
+<script>
+  export default {
+    name: "loyaltyprogram",
+    middleware: ['router-check'] // , 'customer'];
+  }
+</script>
+
+<style scoped>
+
+</style>
